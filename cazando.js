182f690f1a1bbfab3a5b2473ccf6984bae9094f8
jhonnyzaruma95 <jhonnyz1995@hotmail.com>
2026-09-12 16:13:29 -0500

    agrego mas funciones y modifico las variables y constantes

--- cazando.js
+++ cazando.js
@@ -1,8 +1,32 @@
 let canvas=document.getElementById("areaJuego");
 ctx=canvas.getContext("2d");
 
-function graficarGato(){
-    ctx.fillStyle="green";
-    ctx.fillRect(230,200,40,100);
+let gatoX=230;
+let gatoY=200;
+let comidaX=canvas.width-80/2;
+let comidaY=canvas.height-80/2;
+
+const ALTO_GATO=100;
+const ANCHO_GATO=50;
+const ALTO_COMIDA=40;
+const ANCHO_COMIDA=40;
+
+
+
+
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();
 
 }
+
+function graficarGato(){
+    ctx.fillStyle="green";
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
+}
+
+function graficarComida(){
+    ctx.fillStyle="red";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
+}
+
